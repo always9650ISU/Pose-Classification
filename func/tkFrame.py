834f8a5709ee877c8a6812074a8167417dade448
Hsuan <always9650ISU@isu.edy.tw>
2023-01-17 10:37:04 +0800

    add tkFrame.py

--- func/tkFrame.py
+++ func/tkFrame.py
@@ -20,26 +20,39 @@ class tkFrame(tk.Frame):
             self, width=self.width, height=self.height)
         self.canvas.pack()
         self.delay = int(1000 / fps)
+        self.during_time = 0
+        # self.success, self.play_time, self.frame = self.q.get()
+        self.idx = 0
 
         self.update_frame()
 
     def update_frame(self):
-
-        success, play_time, self.frame = self.q.get()
-        if success:
+        a_time = time.time()
+        self.success, self.play_time, self.frame = self.q.get()
+        b_time = time.time()
+        if self.success:
             self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
             self.frame = PIL.ImageTk.PhotoImage(
                 image=PIL.Image.fromarray(self.frame))
             self.canvas.create_image(0, 0, image=self.frame, anchor=tk.NW)
+        c_time = time.time()
+        current = time.time()
+        # if self.name == "Demo":
+        #     # if self.idx <= 180:
+        #     #     print(self.idx, self.name, 1/(current - self.during_time), current -
+        #     #           self.during_time, self.q.qsize(), b_time - a_time, c_time - b_time)
+        #     elapsed = (current - self.start_time) * 1000
+        #     self.delay = max(1, int(self.play_time - elapsed))
 
+        # if self.name == "Camera":
+
+        #     elapsed = (time.time() - self.start_time) * 1000
+        #     self.delay = max(15, int(self.play_time - elapsed))
+        #     print("Camera", self.name, self.q.qsize(), self.delay)
         if self.player:
             audio_frame, val = self.player.get_frame()
-        else:
-            pass
-        if self.name == "Demo":
-            elapsed = (time.time() - self.start_time) * 1000
-            self.delay = max(1, int(play_time - elapsed))
-        if self.name == "Camera":
-            print(self.name, self.q.qsize(), self.delay)
 
+        self.during_time = current
+
+        self.idx += 1
         self.after(self.delay, self.update_frame)
